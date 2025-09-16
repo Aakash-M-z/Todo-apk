05f9e8a40305a5f393b223abe6d4947808bf291e
Aakash-M-z <aakashleo420@gmail.com>
2025-09-16 14:15:29 +0530

    feat: implement database connection and update main application logic

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,7 +1,16 @@
 package com.todo;
 
+import com.todo.util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Todo Application started.");
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            System.out.println("Connected to the database");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/todo/util/DatabaseConnection.java
@@ -0,0 +1,24 @@
+package com.todo.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/todo";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "Aakash@420";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+
+}
